5e7ac38279ee3a35a075385d065a2f222b63bc62
Lluís Vilanova <llvilanova@users.noreply.github.com>
2018-09-25 22:32:33 +0300

    Add set_manual_irqs()

--- spur_ext.py
+++ spur_ext.py
@@ -539,6 +539,125 @@ def set_freq(shell, linux_build=None, cpupower_path=None, freq="max"):
                "-c", "all", "frequency-set", "--freq", freq + "GHz"])
 
 
+def _get_mask(cpu_list):
+    mask = 0
+    for cpu in cpu_list:
+        mask += 1 << cpu
+    return mask
+
+def set_manual_irqs(shell, *irqs, **kwargs):
+    """Make irqbalance ignore the given IRQs, and instead set their SMP affinity.
+
+    Parameters
+    ----------
+    shell
+        Target system.
+    irqs
+        IRQ descriptors.
+    ignore_errors : bool, optional
+        Ignore errors when manually setting an IRQ's SMP affinity. Implies that
+        irqbalance will manage that IRQ. Default is False.
+    irqbalance_banned_cpus : list of int, optional
+        CPUs that irqbalance should not use for balancing.
+    irqbalance_args : list of str, optional
+        Additional arguments to irqbalance.
+
+    Each descriptor in `irqs` is a three-element tuple:
+    * Type: either ``irq`` for the first column in /proc/interrupts, or
+            ``descr`` for the interrupt description after the per-CPU counts.
+    * Regex: a regular expression to apply to the fields above, or `True` to
+             apply to all values (a shorthand to the regex ".*"), or an `int` (a
+             shorthand to the regex "^int_value$").
+    * SMP affinity: list of cpu numbers to set as the IRQ's affinity; if `True`
+                    is used instead, let irqbalance manage this IRQ.
+
+    All matching descriptors are applied in order for each IRQ. If no descriptor
+    matches, or the last matching descriptor has `True` as its affinity value,
+    the IRQ will be managed by irqbalance as before.
+
+    Returns
+    -------
+    The irqbalance process.
+
+    """
+    ignore_errors = kwargs.pop("ignore_errors", False)
+    irqbalance_args = kwargs.pop("irqbalance_args", [])
+    irqbalance_banned_cpus = kwargs.pop("irqbalance_banned_cpus", [])
+    irqbalance_banned_cpus_mask = _get_mask(irqbalance_banned_cpus)
+    if len(kwargs) > 0:
+        raise Exception("unknown argument: %s" % list(kwargs.keys())[0])
+
+    irqs_parsed = []
+    for arg_irq in irqs:
+        if len(arg_irq) != 3:
+            raise ValueError("wrong IRQ descriptor: %s" % repr(arg_irq))
+
+        irq_type, irq_re, irq_cpus = arg_irq
+
+        if isinstance(irq_re, int):
+            irq_re = "^%d$" % irq_re
+        if not isinstance(irq_re, bool) and not isinstance(irq_re, six.string_types):
+            raise TypeError("wrong IRQ descriptor regex: %s" % str(irq_re))
+        if not isinstance(irq_re, bool):
+            irq_re = re.compile(irq_re)
+
+        if (not isinstance(irq_cpus, bool) and (isinstance(irq_cpus, six.string_types) or
+                                              not isinstance(irq_cpus, collections.Iterable))):
+            raise TypeError("wrong IRQ descriptor CPU list: %s" % str(irq_cpus))
+
+        if irq_type not in ["irq", "descr"]:
+            raise ValueError("wrong IRQ descriptor type: %s" % str(irq_type))
+
+        irqs_parsed.append((irq_type, irq_re, irq_cpus))
+
+    irq_manual = []
+    irqbalance_banned = set()
+
+    cre = re.compile(r"(?P<irq>[^:]+):(?:\s+[0-9]+)+\s+(?P<descr>.*)")
+    with shell.open("/proc/interrupts") as f:
+        for line in f.read().split("\n"):
+            match = cre.match(line)
+            if match is None:
+                continue
+
+            irq = match.groupdict()["irq"].strip()
+            descr = match.groupdict()["descr"].strip()
+
+            cpus = True
+
+            for irq_type, irq_cre, irq_cpus in irqs_parsed:
+                if irq_type == "irq":
+                    if irq_cre == True or irq_cre.match(irq):
+                        cpus = irq_cpus
+                elif irq_type == "descr":
+                    if irq_cre == True or irq_cre.match(descr):
+                        cpus = irq_cpus
+                else:
+                    assert False, irq_type
+
+            if cpus != True:
+                irq_manual.append((irq, cpus))
+                irqbalance_banned.add(irq)
+
+    for irq, cpus in irq_manual:
+        mask = _get_mask(cpus)
+        try:
+            shell.run(["sudo", "sh", "-c",
+                       "echo %x > /proc/irq/%s/smp_affinity" % (irq, mask)])
+        except:
+            if ignore_errors:
+                irqbalance_banned.remove(irq)
+            else:
+                raise
+
+    shell.run(["sudo", "service", "irqbalance", "stop"])
+    proc = shell.spawn(["sudo", "IRQBALANCE_BANNED_CPUS=%x" % irqbalance_banned_cpus_mask,
+                        "irqbalance"] + irqbalance_args +
+                       ["--banirq=%s" % banned
+                        for banned in irqbalance_banned])
+    return proc
+
+
 def get_process_pids(shell, pid, filter=None):
     """Get pids of all threads in a given process.
 
